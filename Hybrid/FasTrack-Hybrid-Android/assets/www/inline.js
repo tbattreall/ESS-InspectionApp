8e7ad4b2e2d3e002a53143e310fab3282a0b1133
Jorge Capra <jorge.capra@globant.com>
2012-12-21 10:47:09 -0300

    Added google map and stores table (with geolocation)

--- Hybrid/FasTrack-Hybrid-Android/assets/www/inline.js
+++ Hybrid/FasTrack-Hybrid-Android/assets/www/inline.js
@@ -12,21 +12,60 @@ function fetchStores(){
     forcetkClient.query("SELECT Site_Number__c,Name,geo_latitude__c,geo_longitude__c,Direct_Dial_Phone__c,Site_Address_Line1__c,Site_City__c,Site_State_Province__c,Site_Zip_Code__c FROM Site__c", onSuccessFetchStores, onErrorSfdc); 
 }
 
+var map = null;
+
+function loadPosition(){
+	var geoMap = document.querySelector('#geo-map');
+	map = new google.maps.Map(geoMap, {
+	          zoom: 3,
+	          center: new google.maps.LatLng(37.4419, -94.1419), // United States
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        });
+	map.getDiv().style.border =  '1px solid #ccc';
+	map.getDiv().style.height = "10em";
+
+	navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+/**
+ * Shows the position in the map
+ */
+function showPosition(position) {
+	goToLocation(position.coords.latitude, position.coords.longitude);
+}
+
+function goToLocation(latitude, longitude){
+  var latLng = new google.maps.LatLng(latitude, longitude);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+  map.setCenter(latLng);
+  map.setZoom(12);
+}
+
 function onSuccessFetchStores(response){
     var $j = jQuery.noConflict();
     cordova.require("salesforce/util/logger").logToConsole("onSuccessSfdcContacts: received " + response.totalSize + " stores");
     
-    $j("#div_stores_list").html("")
-    var ul = $j('<ul data-role="listview" data-inset="true" data-theme="a" data-dividertheme="a"></ul>');
-    $j("#div_stores_list").append(ul);
-    
-    ul.append($j('<li data-role="list-divider">Stores: ' + response.totalSize + '</li>'));
-    $j.each(response.records, function(i, contact) {
-           var newLi = $j("<li><a href='#'>" + (i+1) + " - " + contact.Name + "</a></li>");
-           ul.append(newLi);
-           });
-    
-    $j("#div_stores_list").trigger( "create" )
+    var tableData = {};
+	tableData.aaData = response.records;
+	tableData.aoColumns = [{
+		"mData" : "Site_Number__c"
+	}, {
+		"mData" : "Name"
+	}, {
+		"mData" : "Name",
+		"bSearchable": false,
+		"bSortable": false,
+		"mRender" : function(data, type, row) {
+			return '<a href="#" onclick="goToLocation(\''+row.geo_latitude__c+'\',\''+row.geo_longitude__c+'\')">Locate</a>';
+		}
+	}];
+	//tableData.sScrollY = "10em";
+	tableData.sScrollX = "100%";
+
+	$j('#stores_table').dataTable(tableData);
 }
 
 function onErrorSfdc(error) {
